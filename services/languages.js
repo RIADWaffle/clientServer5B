bb9de736efffc996521e0cde0f23a425b66b3295
Roberto <i_criss@live.com.mx>
2022-06-18 21:23:34 -0500

    post changes

--- services/languages.js
+++ services/languages.js
@@ -22,3 +22,83 @@ async function getMultiple(page=1){
 module.exports = {
     getMultiple
 };
+
+async function getUsers(page=1){
+    const offset = helper.getOffset(page, config.listPerPage);
+    const rows = await db.query(
+        `SELECT id, username, password, email, role
+        FROM users LIMIT ${offset}, ${config.listPerPage}
+        `
+    );
+    const data = helper.emptyOrRows(rows);
+    const meta = {page};
+
+    return {
+        data, 
+        meta
+    };
+    
+}
+
+module.exports = {
+    getUsers
+};
+
+async function create(language){
+    console.log(`INSERT INTO languages
+    (name, description, year)
+    VALUES 
+    ('${language.name}','${language.description}',${language.year})
+    `);
+
+    const result = await db.query(
+        `INSERT INTO languages (name, description, year) VALUES
+        ('${language.name}','${language.description}',${language.year})
+        `
+    );
+
+
+    let message = 'Error in creating programming language';
+    if(result.affectedRows){
+        message = 'A new language has been added';
+    }
+
+
+    return {message};
+}
+
+async function update(id,
+    language){
+    const result = await db.query(
+        `UPDATE languages 
+        SET name = '${language.name}',
+        description = '${language.description}',
+        year = '${language.year}'
+        WHERE id = ${id}
+        `
+    );
+    let message = 'Error in updating programming language';
+    if(result.affectedRows){
+        message = 'A language has been updated';
+    }
+
+    return {message};
+}
+
+async function remove(id){
+    const result = await db.query(
+        `DELETE from languages
+        WHERE id = ${id}
+        `
+    );
+    let message = 'Error in deleting programming language';
+    if(result.affectedRows){
+        message = 'A language has been deleted';
+    }
+
+    return {message};
+}
+
+module.exports = {
+    getMultiple, getUsers,create,update,remove
+};
